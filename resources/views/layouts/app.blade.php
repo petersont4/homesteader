3d6ad65272d3bbbfb1e5b8bcb02d68b27f7301d2
librarytech4 <computerboy586@gmail.com>
2026-07-03 20:56:48 -0400

    Fix the navigation hamburger button not working on mobile.
    Closes #4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,12 @@
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-	<input type="checkbox" id="sidebar-toggle" class="peer hidden">
-
+	<div class="group md:flex min-h-screen">
+	<input type="checkbox" id="sidebar-toggle" class="hidden">
 	<label for="sidebar-toggle" class="md:hidden fixed top-4 left-4 z-50 bg-green-700 text-white p-2 rounded cursor-pointer">
 		☰	
 	</label>
-	<div class="md:flex min-h-screen">
-	<nav class="fixed md:relative inset-y-0 left-0 w-64 bg-green-700 text-white p-4 flex flex-col gap-4 transform -translate-x-full transition-transform duration-200 peer-checked:translate-x-0 md:translate-x-0 z-40 pt-20 md:pt-4">
+	<nav class="fixed md:relative inset-y-0 left-0 w-64 bg-green-700 text-white p-4 flex flex-col gap-4 transform -translate-x-full transition-transform duration-200 group-has-[#sidebar-toggle:checked]:translate-x-0 md:translate-x-0 z-40 pt-20 md:pt-4">
 		<span class="font-bold text-lg pl-10 md:pl-0">Homesteader</span>
 		<a href="{{ route('chickens.index') }}" class="hover:underline">All Chickens</a>
 		<a href="{{ route('chickens.create') }}" class="hover:underline">Add Chicken</a>
